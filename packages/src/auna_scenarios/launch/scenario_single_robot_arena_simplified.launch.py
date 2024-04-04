86492d9db5ca4a985d5d32d71fd3ee45540a241b
Harun Teper <harun.teper@tu-dortmund.de>
2024-04-04 16:13:47 +0200

    added first version with devcontainer and package split

---
+++ packages/src/auna_scenarios/launch/scenario_single_robot_arena_simplified.launch.py
@@ -0,0 +1,54 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    """Return launch description"""
+
+    # Package Directories
+    gazebo_pkg_dir = get_package_share_directory('auna_gazebo')
+
+    # Paths to folders and files
+    gazebo_launch_file_dir = os.path.join(gazebo_pkg_dir, 'launch', 'gazebo')
+    spawn_launch_file_dir = os.path.join(gazebo_pkg_dir, 'launch', 'spawn')
+
+    # Launch Argument Configurations
+    world_name = LaunchConfiguration('world_name', default='racetrack_decorated')
+    world_name = LaunchConfiguration('world_name')
+
+    # Launch Arguments
+    world_name_arg = DeclareLaunchArgument(
+        'world_name',
+        default_value='arena',
+        description='Gazebo world file name'
+    )
+
+    # Nodes and other launch files
+    world_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(gazebo_launch_file_dir, 'gazebo_world.launch.py')),
+        launch_arguments={
+            'world_name': world_name
+        }.items(),
+    )
+    spawn_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(spawn_launch_file_dir, 'spawn_single_robot.launch.py')),
+        launch_arguments={
+            'world_name': world_name,
+            'namespace': 'robot',
+            'ground_truth': 'False'
+        }.items(),
+    )
+
+    # Launch Description
+    launch_description = LaunchDescription()
+
+    launch_description.add_action(world_name_arg)
+
+    launch_description.add_action(world_cmd)
+    launch_description.add_action(spawn_cmd)
+
+    return launch_description
